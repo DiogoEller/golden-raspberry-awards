430fe68d97fb8f0d5c8e6e03e7204ea2d3b447bf
DiogoEller <dbrunoeller@hotmail.com>
2024-06-17 19:39:45 -0300

    Ajuste no Controller

--- src/main/java/com/example/goldenraspberryawards/controller/MovieController.java
+++ src/main/java/com/example/goldenraspberryawards/controller/MovieController.java
@@ -5,12 +5,14 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.goldenraspberryawards.model.Movie;
 import com.example.goldenraspberryawards.service.MovieService;
 
 @RestController
+@RequestMapping("/api")
 public class MovieController {
 
     @Autowired
